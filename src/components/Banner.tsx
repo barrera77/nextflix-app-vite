56c8949463493424baa7c69f48d44f0516a30743
Manuel Alva <barrera_ml@hotmail.com>
2023-08-20 11:34:10 -0600

    Fetch movies with axios

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Movie } from "../typings";
+import axios from "axios";
+import { baseUrl } from "../constants/movie";
+import requests from "../utils/requests";
 
 function Banner() {
-  return <div>Banner</div>;
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+
+    async function getNetflixOriginals() {
+      try {
+        const response = await axios.get(requests.fetchNetflixOriginals, {
+          cancelToken: source.token,
+        });
+        const netflixOriginals = response.data.results;
+
+        setMovie(
+          netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+        );
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          // Request was canceled
+        } else {
+          // Handle other errors
+          console.log(error);
+        }
+      }
+    }
+    getNetflixOriginals();
+
+    return () => {
+      // Cancel the Axios request when component unmounts
+      source.cancel();
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+        <img src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`} />
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
---
+++ src/services/api-client.ts
@@ -0,0 +1,35 @@
+/*import axios, { AxiosRequestConfig } from "axios";
+
+export interface FetchResponse<T> {
+  count: number;
+  next: string | null;
+  results: T[];
+}
+
+const axiosInstance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    key: "6b0d00123c4fde0c63391f970dd8920f",
+  },
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((response) => response.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((response) => response.data);
+  };
+}
+
+export default APIClient;*/
